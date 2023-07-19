57de34f0d7008797e92ddec94f8e46184dc95b45
Jon Chesterfield <jonathanchesterfield@gmail.com>
2023-07-19 02:42:12 +0100

    try to avoid claiming there is a file installed

--- lib/cmake/hsa-runtime64/hsa-runtime64Targets-release.cmake
+++ lib/cmake/hsa-runtime64/hsa-runtime64Targets-release.cmake
@@ -7,10 +7,10 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 
 # Import target "hsa-runtime64::hsa-runtime64" for configuration "Release"
 set_property(TARGET hsa-runtime64::hsa-runtime64 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(hsa-runtime64::hsa-runtime64 PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhsa-runtime64.so.1.7.0"
-  IMPORTED_SONAME_RELEASE "libhsa-runtime64.so.1"
-  )
+#set_target_properties(hsa-runtime64::hsa-runtime64 PROPERTIES
+#  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhsa-runtime64.so.1.7.0"
+#  IMPORTED_SONAME_RELEASE "libhsa-runtime64.so.1"
+#  )
 
 list(APPEND _IMPORT_CHECK_TARGETS hsa-runtime64::hsa-runtime64 )
 
